63e1cc0c84bc7143cb6d7c6ed2189813fb76b9c6
Byron Chang <thebyronc@gmail.com>
2017-12-01 11:50:14 -0800

    add scripts to combine values from form

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,25 @@
 $(document).ready(function() {
   $("#trackQuestions").submit(function(event) {
     event.preventDefault();
-    var q1Input = $("input:radio[name=question1]:checked").val();
-    var q2Input = $("input:radio[name=question2]:checked").val();
-    var q3Input = $("input:radio[name=question3]:checked").val();
-    var q4Input = $("input:radio[name=question4]:checked").val();
-    $("#output").text(q1Input + q2Input + q3Input + q4Input);
+    var nameInput = $("input#name").val();
+    var q1Input = parseInt($("input:radio[name=question1]:checked").val());
+    var q2Input = parseInt($("input:radio[name=question2]:checked").val());
+    var q3Input = parseInt($("input:radio[name=question3]:checked").val());
+    var q4Input = parseInt($("input:radio[name=question4]:checked").val());
+    var q5Input = parseInt($("input:radio[name=question5]:checked").val());
+
+    var combinedInput = q1Input + q2Input + q3Input + q14nput + q5Input;
+
+    if (q5Input === 5) {
+      $("#output").text("Hello " + nameInput + ", it is highly recommended that you take the PHP/Drupal course from Epicodus. Based on your response, taking this course will allow you to focus on backend coding.");
+    } else if (q5Input === 1) {
+      $("#output").text("It is highly recommended that you take the PHP/Drupal course from Epicodus. Based on your response, taking this course will allow you to focus on backend coding.");
+    }
   });
 });
+
+// 1-6 = class
+// 7-12 = java
+// PHP/Drupal
+// Java/Android
+// CSS/Design
