54b492d111d0c8aab87bad4a03dd8035e9352950
Byron Chang <thebyronc@gmail.com>
2017-12-01 11:14:21 -0800

    add 4 questions to table, add script to pull values

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,10 @@
-$(document).ready(function(){
-  $("form#trackQuestions").submit(function(event) {
+$(document).ready(function() {
+  $("#trackQuestions").submit(function(event) {
     event.preventDefault();
+    var q1Input = $("input:radio[name=question1]:checked").val();
+    var q2Input = $("input:radio[name=question2]:checked").val();
+    var q3Input = $("input:radio[name=question3]:checked").val();
+    var q4Input = $("input:radio[name=question4]:checked").val();
+    $("#output").text(q1Input + q2Input + q3Input + q4Input);
   });
 });
