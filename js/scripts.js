ee0540cebe834e9fdcf38d0c0cf43161f111f27b
Byron Chang <thebyronc@gmail.com>
2017-12-01 12:04:31 -0800

    add branching to determine which course the user should take

--- js/scripts.js
+++ js/scripts.js
@@ -8,18 +8,24 @@ $(document).ready(function() {
     var q4Input = parseInt($("input:radio[name=question4]:checked").val());
     var q5Input = parseInt($("input:radio[name=question5]:checked").val());
 
-    var combinedInput = q1Input + q2Input + q3Input + q14nput + q5Input;
+    var combinedInput = q1Input + q2Input + q3Input + q4Input + q5Input;
 
     if (q5Input === 5) {
       $("#output").text("Hello " + nameInput + ", it is highly recommended that you take the PHP/Drupal course from Epicodus. Based on your response, taking this course will allow you to focus on backend coding.");
-    } else if (q5Input === 1) {
-      $("#output").text("It is highly recommended that you take the PHP/Drupal course from Epicodus. Based on your response, taking this course will allow you to focus on backend coding.");
+    } else if (combinedInput <= 14) {
+      $("#output").text("Hello " + nameInput + ", It is recommended that you take the CSS/Design course from Epicodus. Based on your response, taking this course will allow you to focus on developing for a web interface.");
+    } else if (combinedInput >= 16) {
+      $("#output").text("Hello " + nameInput + ", It is recommended that you take the Java/Android course from Epicodus. Based on your response, taking this course will allow you to focus on developing for a mobile interface.");
+    } else if (combinedInput === 15) {
+      $("#output").text("Hello " + nameInput + ", You seem to not seem to have a particular interest in either mobile development or desktop development. May we suggest the PHP/Drupal course from Epicodus.");
+    } else {
+      console.log("else")
     }
   });
 });
 
-// 1-6 = class
-// 7-12 = java
+// 5-14 = class
+// 16-25 = java
 // PHP/Drupal
 // Java/Android
 // CSS/Design
